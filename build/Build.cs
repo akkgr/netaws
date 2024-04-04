980393260a9de91d872be7c84e224a6ce393e4ec
akkggr <akkgr@users.noreply.github.com>
2024-04-04 20:13:58 +0300

    wip

--- build/Build.cs
+++ build/Build.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO.Compression;
 using System.Linq;
 
 using Nuke.Common;
@@ -7,15 +8,14 @@ using Nuke.Common.Execution;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.Pulumi;
+using Nuke.Common.Utilities.Collections;
 
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using System.IO.Compression;
 
 class Build : NukeBuild
 {
